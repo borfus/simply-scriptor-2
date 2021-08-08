37409ebd99e003259d201401705764c18a61af36
borfus <mrpeteyb@gmail.com>
2021-08-07 23:56:32 -0500

    Beginning progress on recording prototype.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,71 @@
-use rdev::{simulate, Button, EventType, Key, SimulateError};
-use std::{thread, time};
+use rdev::{listen, simulate, Event, EventType, SimulateError, Key};
+use std::{thread, time, time::SystemTime, sync::Arc, sync::Mutex, sync::mpsc::channel};
 
 fn main() {
-    send(&EventType::MouseMove { x: 200.0, y: 200.0 });
-    send(&EventType::ButtonPress(Button::Left));
-    send(&EventType::ButtonRelease(Button::Left));
-    send(&EventType::KeyPress(Key::KeyS));
-    send(&EventType::KeyRelease(Key::KeyS));
+    // spawn new thread because listen blocks
+    let (sendch, recvch) = channel();
+    let _listener = thread::spawn(move || {
+        listen(move |event| {
+            sendch.send(event)
+                .unwrap_or_else(|e| println!("Could not send event {:?}", e));
+        })
+        .expect("Could not listen");
+    });
+
+    let record = Arc::new(Mutex::new(false));
+    let run = Arc::new(Mutex::new(false));
+    let events = Arc::new(Mutex::new(Vec::new()));
+
+    let events_ref = Arc::clone(&events);
+    let record_ref = Arc::clone(&record);
+    thread::spawn(move || {
+        for event in recvch.iter() {
+            // println!("Received {:?}", event.event_type);
+            let mut record_ref = record_ref.lock().unwrap();
+            if event.event_type == EventType::KeyRelease(Key::F10) {
+                *record_ref = !*record_ref;
+                if *record_ref {
+                    println!("Recording...");
+                } else {
+                    println!("Stopped recording...");
+                }
+            }
+
+            if *record_ref {
+                events_ref.lock().unwrap().push(event);
+            }
+        }
+    });
+
+    loop {
+        let record = record.lock().unwrap();
+        let mut run = run.lock().unwrap();
+        let mut events = events.lock().unwrap();
+
+        if *run && !*record {
+            send_events(events.to_vec());
+            *run = false;
+        }
+
+        for event in events.to_vec() {
+            if *record {
+                println!("{:?}", event.event_type);
+            }
+        }
+        events.clear();
+    };
 }
 
-fn send(event_type: &EventType) {
+fn send_events(events: Vec<Event>) {
+    let wait_duration = SystemTime::now();
+    for event in events {
+        send_event(&event.event_type);
+        let wait_duration = event.time.duration_since(wait_duration);
+        thread::sleep(wait_duration.unwrap());
+    }
+}
+
+fn send_event(event_type: &EventType) {
     let delay = time::Duration::from_millis(20);
     match simulate(event_type) {
         Ok(()) => (),
